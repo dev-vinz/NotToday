5859c0f3c36d374e1ec486dbf1d3d33b823ec43f
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-05-11 15:42:55 +0200

    dev(TimeSpan) : Update and optimize

--- Sources/tools/utils.cpp
+++ Sources/tools/utils.cpp
@@ -4,9 +4,29 @@
 |*                          PUBLIC METHODS                           *|
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+QString Utils::dateFormat()
+{
+    return "dd/MM/yyyy";
+}
+
+QString Utils::dateRegex()
+{
+    return "(0[1-9]|[12][0-9]|3[01])/(0[1-9]|[1][0-2])/(2[0-1][0-9][0-9])";
+}
+
 QString Utils::dateToString(QDateTime date)
 {
     return date.toString(Utils::dateFormat());
+}
+
+QString Utils::timeFormat()
+{
+    return "dd:HH:mm";
+}
+
+QString Utils::timeRegex()
+{
+    return "((0[0-9]|1[0-9]|2[0-9]|3[0-1]):([0-1][0-9]|2[0-4]):([0-5][0-9]|6[0]))";
 }
 
 TimeSpan Utils::stringToTime(QString date)
@@ -20,23 +40,4 @@ TimeSpan Utils::stringToTime(QString date)
     return TimeSpan::fromDays(day)
             .add(TimeSpan::fromHours(hour))
             .add(TimeSpan::fromMinutes(minute));
-}
-
-QString Utils::timeFormat()
-{
-    return "dd:HH:mm";
-}
-
-QString Utils::dateFormat()
-{
-    return "dd/MM/yyyy";
-}
-QString Utils::timeRegex()
-{
-    return "((0[0-9]|1[0-9]|2[0-9]|3[0-1]):([0-1][0-9]|2[0-4]):([0-5][0-9]|6[0]))";
-}
-
-QString Utils::dateRegex()
-{
-    return "(0[1-9]|[12][0-9]|3[01])/(0[1-9]|[1][0-2])/(2[0-1][0-9][0-9])";
 }
