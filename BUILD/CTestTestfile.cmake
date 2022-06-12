6dcc90d2225eacc54a5927fece566aedcaaeff55
Guillaume Mouchet <guillaume.mouchet@he-arc.ch>
2022-06-12 22:55:05 +0200

    ajout de google tests2

--- BUILD/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/DEV/Qt/not-today/Sources
-# Build directory: C:/DEV/Qt/not-today/BUILD
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/DEV/Qt/not-today/BUILD/tests[1]_include.cmake")
-subdirs("tools")
-subdirs("_deps/googletest-build")
